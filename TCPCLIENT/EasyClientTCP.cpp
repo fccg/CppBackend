bc2af911a0e0749bf7f5aed8c2141a8e64dd2f8a
fccg <iamlfk@163.com>
2021-09-16 00:59:05 +0800

    TCP Server Client first commit

---
+++ TCPCLIENT/EasyClientTCP.cpp
@@ -0,0 +1,84 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <winsock2.h>
+// #pragma comment(lib,"ws2_32.lib")
+#include <stdio.h>
+
+
+using namespace std;
+
+int main()
+{
+    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // int i = 0;
+    // for (const string& word : msg)
+    // {
+    //     cout << word << " ";
+    //     ++i;
+    // }
+    // cout << endl;
+
+
+
+    WORD ver = MAKEWORD(2,2);
+    WSADATA dat;
+    WSAStartup(ver,&dat);
+
+    // 1建立一个socket套接字
+    SOCKET _sock = socket(AF_INET,SOCK_STREAM,0);
+    if (INVALID_SOCKET == _sock){
+        printf("construct error\n");
+    }else{
+        printf("construct success\n");
+    }
+
+    // 2连接服务器
+    sockaddr_in _sin = {};
+    _sin.sin_family = AF_INET;
+    _sin.sin_port = htons(4567);
+    _sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    int ret = connect(_sock,(sockaddr*)&_sin,sizeof(sockaddr_in));
+    if(SOCKET_ERROR == ret){
+        printf("connect to server error\n");
+    }else{
+        printf("connect to server success\n");
+    }
+
+    
+    while(true){
+        //输入请求
+        char cmdBuf[128] = {};
+        printf("Please enter a command:");
+        scanf("%s",cmdBuf);
+        // 4处理请求
+        if(0 == strcmp(cmdBuf,"exit")){
+            printf("The command entered this time is exit\n");
+            break;
+        }else{
+            // 5将命令发送给服务器
+            send(_sock,cmdBuf,strlen(cmdBuf)+1,0);
+        }
+
+        // 6接收服务器信息
+        char recvBuf[128] = {};
+        int nlen = recv(_sock,recvBuf,128,0);
+        if(nlen > 0 ){
+            printf("server data:%s\n",recvBuf);
+        }
+    }
+
+    // 7关闭
+    closesocket(_sock);
+
+    WSACleanup();
+    printf("mission over\n");
+    getchar();
+
+    return 0;
+}
---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,103 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <winsock2.h>
+#include <stdio.h>
+
+// #pragma comment(lib,"ws2_32.lib")
+
+
+using namespace std;
+
+int main()
+{
+    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // int i = 0;
+    // for (const string& word : msg)
+    // {
+    //     cout << word << " ";
+    //     ++i;
+    // }
+    // cout << endl;
+
+
+
+    WORD ver = MAKEWORD(2,2);
+    WSADATA dat;
+    WSAStartup(ver,&dat);
+
+    // 1建立一个socket套接字
+    SOCKET _sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    // 2绑定端口
+    sockaddr_in _sin = {};
+    _sin.sin_family = AF_INET;
+    _sin.sin_port = htons(4567);
+    _sin.sin_addr.S_un.S_addr = INADDR_ANY;
+    if(bind(_sock,(sockaddr*)&_sin,sizeof(sockaddr_in)) == SOCKET_ERROR){
+        printf("bind ERROR\n");
+    }else{
+        printf("bind success\n");
+    }
+    // 3监听网络
+    if(listen(_sock,5) == SOCKET_ERROR){
+        printf("listen ERROR\n");
+    }else{
+        printf("listen success\n");
+    }
+
+    // 4等待客户端连接
+    sockaddr_in clientAddr = {};
+    int nAddLen = sizeof(sockaddr_in);
+    SOCKET _cSock = INVALID_SOCKET;
+    
+
+    _cSock = accept(_sock,(sockaddr*)&clientAddr,&nAddLen);
+    if(INVALID_SOCKET == _cSock){
+        printf("ERROR client sock");
+    }
+    printf("new client:socket = %d,IP = %s \n",(int)_cSock, inet_ntoa(clientAddr.sin_addr));
+
+    char _recvBuf [128] = {};
+    while (true)
+    {   
+        //5接收客户端请求数据
+        int nlen = recv(_cSock,_recvBuf,128,0);
+        if(nlen <= 0){
+            printf("client exit,mission over\n");
+            break;
+        }
+        printf("client message:%s \n",_recvBuf);
+        //6 处理请求
+        if(0 == strcmp(_recvBuf,"getName")){
+            char msgBuf [] = "sucker";
+            // 7向客户端发送一条数据
+            send(_cSock,msgBuf,strlen(msgBuf)+1,0);
+
+        }else if (0 == strcmp(_recvBuf,"getAge"))
+        {
+            char msgBuf [] = "213";
+            // 7向客户端发送一条数据
+            send(_cSock,msgBuf,strlen(msgBuf)+1,0);
+        }else{
+            char msgBuf [] = "surprise motherfucker";
+            //7 向客户端发数据
+            // 加上结尾符
+            send(_cSock,msgBuf,strlen(msgBuf)+1,0);
+        }
+        
+        
+    }
+ 
+    // 8关闭
+    closesocket(_sock);
+
+    WSACleanup();
+    printf("mission over\n");
+    getchar();
+    return 0;
+}
